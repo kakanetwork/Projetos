aabfa0e2067d85768fcac9abb5e5596e59e7900d
kakanetwork <kalvimklain@gmail.com>
2022-12-25 22:19:05 -0300

    Projetos Faculdade

--- STRINGS/Q10.py
+++ STRINGS/Q10.py
@@ -1,53 +1,53 @@
-x = int(input('\nInforme a coordenada inicial de x: '))
-y = int(input('Informe a coordenada inicial de y: '))
+x = int(input('\nInforme a Coordenada inicial de X: '))
+y = int(input('Informe a Coordenada inicial de Y: '))
 var1, var2 = x, y
-qnt_val = 0
+movi_val = 0
 u, d, r, l, o, n, e, w = 0, 0, 0, 0, 0, 0, 0, 0
 while True:
     print('\n',end='='*100)
-    movimento = str(input('\n\nInsira para onde o robô deve se deslocar: '))
-    movimento = movimento.lower()
-    if movimento == 'u':
+    movi = str(input('\n\nInsira para onde o robô deve se deslocar: '))
+    movi = movi.upper()
+    if movi == 'U':
         y += 1
-        qnt_val += 1
+        movi_val += 1
         u += 1
-    elif movimento == 'd':
+    elif movi == 'D':
         y -= 1
-        qnt_val += 1
+        movi_val += 1
         d += 1
-    elif movimento == 'r':
+    elif movi == 'R':
         x += 1
-        qnt_val += 1
+        movi_val += 1
         r += 1
-    elif movimento == 'l':
+    elif movi == 'L':
         x -= 1
-        qnt_val += 1
+        movi_val += 1
         l += 1
-    elif movimento == 'o':
+    elif movi == 'O':
         y += 1
         x -= 1
-        qnt_val += 1
+        movi_val += 1
         o += 1
-    elif movimento == 'n':
+    elif movi == 'N':
         y += 1
         x +=1
-        qnt_val += 1
+        movi_val += 1
         n += 1
-    elif movimento == 'e':
+    elif movi == 'E':
         y -= 1
         x += 1
-        qnt_val += 1
+        movi_val += 1
         e += 1
-    elif movimento == 'w':
+    elif movi == 'W':
         y -= 1
         x -= 1
-        qnt_val += 1
+        movi_val += 1
         w += 1
     else:
         print('\nInforme uma letra válida!\n-> U (cima) - D (baixo) - R (direita) - L (esquerda)')
         print('-> O (cima-esquerda) - N (cima-direita) - E (baixo-direita) e W (baixo-esquerda)\n')
     print(f'A posição Inicial foi -> {var1, var2}\nA posição Final é -> {x,y}')
-    print(f'Quantidade de Movimentos Válidos -> {qnt_val}')
+    print(f'Quantidade de Movimentos Válidos -> {movi_val}')
     print(f'Os movimentos válidos executados foram:\n{u}x Cima, {d}x Baixo, {r}x Direita, {l}x Esquerda, {o}x Noroeste, {n}x Nordeste, {e}x Sudeste, {w}x Sudoeste')
     if var1 > 0 and var2 > 0:
          print(f'O robô Começou no primeiro quadrante nas coordenadas -> {var1,var2}')
--- STRINGS/Q2.py
+++ STRINGS/Q2.py
@@ -1,5 +1,4 @@
 var = str(input('Digite uma frase: '))
-
-print(f'A frase que você inseriu foi: {var}\n'); print('Corrigindo os espaços vazios...\n')
+print(f'\nA frase foi: {var}')
 var = var.replace(' ', '_')
-print(f'A frase corrigida é: {var}'); print('\nFim!')
+print(f'A frase corrigida é: {var}\n')
